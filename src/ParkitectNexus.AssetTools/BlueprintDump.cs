570bc3c3a063db7f9dad6269e9d92f78762973ed
Tim Potze <timpotze@gmail.com>
2016-05-14 21:04:11 +0200

    Changed AssetMagic to be a submodule

--- src/ParkitectNexus.AssetTools/BlueprintDump.cs
+++ src/ParkitectNexus.AssetTools/BlueprintDump.cs
@@ -14,6 +14,7 @@
 // limitations under the License.
 
 using System;
+using System.Linq;
 using ParkitectNexus.AssetMagic;
 using ParkitectNexus.AssetMagic.Data.Blueprints;
 using ParkitectNexus.AssetMagic.Data.Coasters;
@@ -25,38 +26,12 @@ namespace ParkitectNexus.AssetTools
         public BlueprintDump(IBlueprint blueprint)
         {
             if (blueprint == null) throw new ArgumentNullException(nameof(blueprint));
-            var h = blueprint.Header;
-            Header = new BlueprintHeader {
-                ActiveMods = h.ActiveMods,
-                ApproximateCost = h.ApproximateCost,
-                DecoTypes = h.DecoTypes,
-                FlatRideTypes = h.FlatRideTypes,
-                ManufacturerName = h.ManufacturerName,
-                Name = h.Name,
-                TrackedRideTypes = h.TrackedRideTypes ?? h.Types,
-                Types = h.Types,
-                Date = h.Date,
-                GameVersion = h.GameVersion,
-                GameVersionName = h.GameVersionName,
-                SavegameVersion = h.SavegameVersion,
-                Type = h.Type
-            };
+            Header = blueprint.Header;
 
-            var c = blueprint.GetElement<Coaster>();
-            Coaster = c == null ? null : new Coaster {
-                CarType = c.CarType,
-                Duration = c.Duration,
-                EntranceFee = c.EntranceFee,
-                Id = c.Id,
-                Position = c.Position,
-                Rotation = c.Rotation,
-                Stats = c.Stats,
-                Track = c.Track,
-                TrainCount = c.TrainCount,
-                TrainLength = c.TrainLength,
-                WaitTime = c.WaitTime,
-                Type = c.Type
-            };
+            if (Header.TrackedRideTypes == null)
+                Header.TrackedRideTypes = Header.Types;
+
+            Coaster = blueprint.Coasters.FirstOrDefault();
         }
 
         public BlueprintHeader Header { get; set; }
--- src/ParkitectNexus.AssetTools/SavegameDump.cs
+++ src/ParkitectNexus.AssetTools/SavegameDump.cs
@@ -25,45 +25,15 @@ namespace ParkitectNexus.AssetTools
         {
             if (savegame == null) throw new ArgumentNullException(nameof(savegame));
            
-            var h = savegame.Header;
-            Header = new SavegameHeader {
-                ActiveMods = h.ActiveMods,
-                GuestCount = h.GuestCount,
-                Money = h.Money,
-                Name = h.Name,
-                ParkDate = h.ParkDate,
-                ParkRating = h.ParkRating,
-                Screenshot = h.Screenshot,
-                TimePlayed = h.TimePlayed,
-                Date = h.Date,
-                GameVersion = h.GameVersion,
-                GameVersionName = h.GameVersionName,
-                SavegameVersion = h.SavegameVersion,
-                Type = h.Type
-            };
-
-            var p = savegame.Park;
-            Park = new Park {
-                Guid = p.Guid,
-                Id = p.Id,
-                ParkInfo = p.ParkInfo,
-                ParkName = p.ParkName,
-                Patches = p.Patches,
-                SendGuestsHome = p.SendGuestsHome,
-                Settings = p.Settings,
-                SpawnedAtTime = p.SpawnedAtTime,
-                XSize = p.XSize,
-                YSize = p.YSize,
-                ZSize = p.ZSize,
-                Type = p.Type
-            };
-            GuestCount = (int)h.GuestCount;
+            Header = savegame.Header;
+            Park = savegame.Park;
+            GuestCount = savegame.GuestCount;
         }
 
         public SavegameHeader Header { get; set; }
 
         public Park Park { get; set; }
 
-        public int GuestCount { get; set; }
+        public long GuestCount { get; set; }
     }
 }
